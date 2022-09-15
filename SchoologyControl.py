053040ae32325647b99d5b25a9c95bb5b18e4cb5
Aaron Zech <aaronzech@gmail.com>
2022-09-15 09:01:21 -0500

    Updated to no longer need AutoIT

--- SchoologyControl.py
+++ SchoologyControl.py
@@ -19,26 +19,40 @@ def SchoologyControl(browser,groupName,autoITScript):
     enrollmentType.click()
     enrollmentType.send_keys('m')
     enrollmentType.click()
+    enrollmentType.send_keys(Keys.TAB)
 
     # Select Group to update
-    select = Select(browser.find_element(By.ID,'edit-group-nid'))
+ 
+    
+    
+    
+    select = browser.find_element(By.ID, "s2id_autogen1_search")
+    select.send_keys('PC CRC & Counseling')
+    time.sleep(5)
+    select.send_keys(Keys.ENTER)
     # select by visible text
-    select.select_by_visible_text('PC CRC & Counseling')
     time.sleep(0.2)
     print("group selected")
 
     # check clear group enrollments
-    clearExistingEnrollments = browser.find_element(By.ID,'edit-clear-enrollments')
-    clearExistingEnrollments.click()
+    #clearExistingEnrollments = browser.find_element(By.ID,'edit-clear-enrollments')
+    #clearExistingEnrollments.click()
 
     time.sleep(2)  # delay for more reliablitiy
     # uploadFIle
-    attachFile = browser.find_element(By.ID,'upload-btn')
-    attachFile.click()
+
+    #Send file keys method
+    file = browser.find_element(By.XPATH,"//*[contains(@id,'html5')][not(contains(@id, '_container'))]") # Dynamic Element - Look for 'HTML5' and does not contain 'container'
+    file.send_keys("C:\\Users\\zechaaron\\OneDrive - Osseo Area Schools\\Documents\\AD\\PCSH_Students.csv")
+    #------------------------
+
+    #attachFile = browser.find_element(By.ID,'upload-btn')
+    #attachFile.click()
     # Call AutoIT
-    autoit.run("C:\\Users\\zechaaron\\Downloads\\Schoology-Group-Automation-master\\Schoology-Group-Automation-master\\FileUpload.au3")
+    #autoit.run("C:\\Users\\zechaaron\\Downloads\\Schoology-Group-Automation-master\\Schoology-Group-Automation-master\\FileUpload.exe")
     #autoit.run("C:\\Users\\zechaaron\\Downloads\\Schoology-Group-Automation-master\\Schoology-Group-Automation-master\\"+autoITScript);
-    # 
+    #
+
     # Submit
     time.sleep(8)
     submitButton = browser.find_element(By.ID,'edit-submit').click()
