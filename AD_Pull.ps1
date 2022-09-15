fe5b1f031a6f117fa0fbc8ddefe7288015a924fc
Aaron Zech <aaronzech@gmail.com>
2022-09-15 09:03:04 -0500

    Updated to match new AD structure

--- AD_Pull.ps1
+++ AD_Pull.ps1
@@ -1,4 +1,4 @@
 ﻿Import-Module ActiveDirectory
-Get-ADUser -SearchBase "OU=High,OU=Schools,OU=AllUsers,DC=core,DC=oas,DC=ld" -Filter * -Properties DisplayName, Company, Created,Department, LastLogonDate, OfficePhone,physicalDeliveryOfficeName, EmailAddress | 
+Get-ADUser -SearchBase "OU=High School,OU=Schools,OU=AllUsers,DC=core,DC=oas,DC=ld" -Filter * -Properties DisplayName, Company, Created,Department, LastLogonDate, OfficePhone,physicalDeliveryOfficeName, EmailAddress | 
     Select-Object DisplayName, SamAccountName, Company, Created, LastLogonDate,Department, OfficePhone,physicalDeliveryOfficeName, EmailAddress, Enabled |
-    Export-Csv C:\Users\zechaaron\Documents\AD\ADUsers.csv
+    Export-Csv C:\Users\zechaaron\Downloads\ADUsers.csv
